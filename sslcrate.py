1527c1f6a356b2731e7f30041f78e5cd5e8df50d
Jesse Ward <jesse.ward@gmail.com>
2023-04-06 17:27:58 +0000

    removing ollllddddddd cruft.

--- sslcrate.py
+++ sslcrate.py
@@ -2,88 +2,88 @@
 
 import mmap
 import struct
-import sys
 from optparse import OptionParser
 
+
 def readtag(tag):
-    """ Returns true of the requested tag was found at the next position in the 
-        the mssldb """
+    """Returns true of the requested tag was found at the next position in the
+    the mssldb"""
 
     currpos = ssldb.tell()
-    foundtag =  ssldb.read(len(tag))
+    foundtag = ssldb.read(len(tag))
 
     if foundtag == tag:
         return True
     else:
         ssldb.seek(currpos)
-     
+
+
 def readbytes(numbytes):
-    """ Reads a variable length of bytes from the mssldb """
+    """Reads a variable length of bytes from the mssldb"""
 
     return ssldb.read(numbytes)
 
+
 def readvarlenstr(pad=4):
-    """ Reads and parses a variable length string field """
-    
+    """Reads and parses a variable length string field"""
+
     varlen = ssldb.read(4)
     varlen = struct.unpack(">L", varlen)
     return ssldb.read(varlen[0])
 
 
 p = OptionParser()
-p.add_option("-f", "--file", dest="dbfile",
-             help="Serate Create .db file to read")
+p.add_option("-f", "--file", dest="dbfile", help="Serate Create .db file to read")
 
 (opt, args) = p.parse_args()
 
 if not opt.dbfile:
     p.error("Please specify the SSL crate file to read.")
 
-f = open(opt.dbfile, 'r+b')
+f = open(opt.dbfile, "r+b")
 
 ssldb = mmap.mmap(f.fileno(), 0)
 
 while ssldb.tell() < ssldb.size():
-
     # read VRSN header data
-    if readtag('vrsn'):
+    if readtag("vrsn"):
         vrsn = (ssldb.tell(), readbytes(60))
-        print "vrsn %.5d : %s" % (vrsn[0], vrsn[1])
+        print("vrsn %.5d : %s" % (vrsn[0], vrsn[1]))
 
     # handle column sorting tags
-    elif readtag('osrt'):
+    elif readtag("osrt"):
         osrt = (ssldb.tell(), readbytes(4))
-        print "osrt %.5d : %s" % (osrt[0], osrt[1])
-        
-        # read sorted column name header
-        if readtag('tvcn'):
-            tvcn = (ssldb.tell(), readvarlenstr())
-            print "tvcn %.5d : %s" % (tvcn[0], tvcn[1])
+        print("osrt %.5d : %s" % (osrt[0], osrt[1]))
 
-        if readtag('brev'):
+        # read sorted column name header
+        if readtag("tvcn"):
+            tvcn = (ssldb.tell(), readvarlenstr())
+            print("tvcn %.5d : %s" % (tvcn[0], tvcn[1]))
+
+        if readtag("brev"):
             brev = (ssldb.tell(), readbytes(5))
-            print brev
+            print(brev)
 
     # read in the remaining available column data.
-    elif readtag('ovct'):
+    elif readtag("ovct"):
         ovct = (ssldb.tell(), readbytes(4))
-        print ovct
+        print(ovct)
 
-        if readtag('tvcn'):
+        if readtag("tvcn"):
             tvcn = (ssldb.tell(), readvarlenstr())
-            print "tvcn %.5d : %s" % (tvcn[0], tvcn[1])
+            print("tvcn %.5d : %s" % (tvcn[0], tvcn[1]))
 
-        if readtag('tvcw'):
+        if readtag("tvcw"):
             tvcw = (ssldb.tell(), readbytes(6))
-            print tvcw
+            print(tvcw)
 
     # read in track data.
-    elif readtag('otrk'):
+    elif readtag("otrk"):
         otrk = (ssldb.tell(), readbytes(4))
         tracksize = struct.unpack(">L", otrk[1])
-        print "otrk %.5d : %s" % (otrk[0], otrk[1])
+        print("otrk %.5d : %s" % (otrk[0], otrk[1]))
 
-        if readtag('ptrk'):
+        if readtag("ptrk"):
             ssldb.read(4)
-            track = (ssldb.tell(), readbytes(tracksize[0]-8))
-            print "track %.5d : %s" % (track[0], track[1])
+            track = (ssldb.tell(), readbytes(tracksize[0] - 8))
+            print("track %.5d : %s" % (track[0], track[1]))
